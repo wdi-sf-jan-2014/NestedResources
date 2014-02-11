45c481d17e78c7a6075f90ab6bb916c3ff82cd76
Alex Notov <anotov@gmail.com>
2014-02-11 13:46:01 -0800

    Initial rails app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NestedResources::Application.config.secret_key_base = '2c718a633dda2aa7924c3fddc6431e5a444b52d594fb7e7712200d3d6d9a8ef777b8acb75caf5c58737aa92c751b776adf95092d4a80aff9f64d5f2be83111e3'
