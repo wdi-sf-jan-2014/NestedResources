148a3434a515027c3073d9f4dad938d5997dceb2
smarsh25 <sjm.atl@gmail.com>
2014-02-11 20:09:21 -0800

    adding controllers, without additional test framework

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 NestedResources::Application.routes.draw do
 
+	# Listing routes for resources, also limiting routes to only
+	# those needed (index, new, create, show)
 	resources :posts, only: [:index, :show, :new, :create] do
   	resources :comments, only: [:index, :show, :new, :create]
 	end
 
+	# Create a root route as well
   root to: 'posts#index'
 
+  # Routes needed for devise / user authentication
   devise_for :users
 
 end
