934ba6add39149d78602478880a99fb67c1d69db
smarsh25 <sjm.atl@gmail.com>
2014-02-11 23:33:59 -0800

    adding create comment, captures and persists, need to add show

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,20 @@ class CommentsController < ApplicationController
   def index
   end
 
+  # GET, start to create a comment for a given post (collect attributes)
   def new
+  	@post = Post.find(params[:post_id])
+  	@comment = @post.comments.new
   end
 
+  # POST, create a comment for a given post (persist attributes)
   def create
+  	new_comment = params.require(:comment).permit(:body)
+  	post = Post.find(params[:post_id])
+  	comment = post.comments.create(new_comment)
+
+  	# redirect_to post_comment_path(post, comment)
+  	redirect_to '/'
   end
 
   def show
