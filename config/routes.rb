6cac008732edc99992bb4df90a3fafc44cb30ffa
simrankg1 <andrea.gill@gmail.com>
2014-02-12 15:59:31 -0800

    second version

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,24 @@
 NestedResources::Application.routes.draw do
-  get "posts/index"
+  # get "comments/new"
+  # get "comments/index"
+  # get "comments/create"
+  # get "comments/show"
+  # get "posts/index"
   devise_for :users
 
   get '/posts', to: 'posts#index', as: :posts
   root to: 'posts#index'
+
+  resources :posts do 
+  	resources :comments 
+  end
+  
 end
+
+#CAN ALSO DO THIS: 
+#devise_for :users
+
+# resources :posts, only: [:index], :new, :create, :show]
+# resources :comments, only: [:show]
+#root to: 'posts#index'
+
