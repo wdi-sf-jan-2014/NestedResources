92b707b867bfde0f3121457c1e6f37cabb0a625b
smarsh25 <sjm.atl@gmail.com>
2014-02-11 22:41:24 -0800

    adding first pass at creating new post, no comments, no display/show of post yet

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,15 @@ class PostsController < ApplicationController
   def index
   end
 
+  # GET, start of creating a new Post (obtain attributes)
   def new
+  	@post = Post.new
   end
 
+  # POST, finalize the creation of new Post (persist attributes)
   def create
+  	post = Post.create(params[:post].permit(:body, :link))
+  	redirect_to '/'
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
 	has_many :comments
+
+	# accept the associate comments, as long as they are not blank
+	accepts_nested_attributes_for :comments, reject_if: proc { |attributes| attributes['title'].blank? }
 end
