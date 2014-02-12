c8986be042f79ba04a40a4de33024e33948036a8
Tanner Swenson <TannerSwenson@gmail.com>
2014-02-11 19:21:01 -0800

    Saving just in case cause a major error

---
+++ app/models/comment.rb
@@ -0,0 +1,6 @@
+class Comment < ActiveRecord::Base
+  # belongs_to :post
+  # belongs_to :comment
+  # belongs_to :user
+  # has_many :comments
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,12 @@ NestedResources::Application.routes.draw do
 
   get '/posts', to: 'posts#index', as: :posts
   root to: 'posts#index'
+
+  #  resources :posts do
+  #   resources :comments
+  # end
+
+  # resources :comments do
+  #   resources :comments
+  # end
 end
