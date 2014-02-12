e5511b94c074b5e64f63ae760e205743871bd0a2
alanvp <apvanpelt@yahoo.com>
2014-02-12 13:23:24 -0800

    added add comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,24 @@
 class CommentsController < ApplicationController
   def new
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build
   end
 
   def show
-    post_id = params[:post_id]
-    @post = Post.find(post_id)
-    comment_id = params[:id]
-    @comment = Comment.find(comment_id)
+     post_id = params[:post_id]
+     @post = Post.find(post_id)
+     comment_id = params[:id]
+     @comment = Comment.find(comment_id)
+
   end
+
+  def create
+    new_comment = params.require(:comment).permit(:body)
+    post = Post.find(params[:post_id])
+    comment = post.comments.create(new_comment)
+
+    redirect_to post_comment_path(post, comment)
+  end
+
+
 end
