444f0fc2a7091bc7adf682f9b7b56880f79c7fc9
JSchulhof <JSchulhof@gmail.com>
2014-02-12 21:46:47 -0800

    fixed bugs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,21 @@ class PostsController < ApplicationController
 
   def create
   	post = Post.create(params[:post].permit(:link,:body, :comments_attributes=>[:body]))
-    LinksWorker.perform_async(post.id)
+
+    
+    post2 = Post.find(post.id)
+    link = post2.link
+      #uri = URI.parse(link)
+      #if uri.scheme != "http"
+      # link = "http://"+link
+      #end
+    response = Typhoeus.get(link)
+    binding.pry
+    if response.response_code > 399 || response.response_code == 0
+          post2.destroy
+    end
+
+    #LinksWorker.perform_async(post.id)
   	redirect_to post_path(post)
   end
 
--- app/workers/links_worker.rb
+++ app/workers/links_worker.rb
@@ -3,19 +3,18 @@ class LinksWorker
 
   def perform(post_id)
     require 'open-uri'
+    
     post = Post.find(post_id)
-    if (post)
     	link = post.link
-	    uri = URI.parse(link)
-	    if uri.scheme != "http"
-	    	link = "http://"+link
-	    end
+	    #uri = URI.parse(link)
+	    #if uri.scheme != "http"
+	    #	link = "http://"+link
+	    #end
 	     response = Typhoeus.get(link)
-	     if response.response_code > 399
+	     if response.response_code > 399 || response_code == 0
 	        post.destroy
 	     end
 	       
-	end #if
   end #method
 
 
