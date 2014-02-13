6d97c900521b33dc66f9598f85c41ef7638b83d1
mbetts7 <mbetts7@gmail.com>
2014-02-12 18:42:16 -0800

    stacks on stacks comments working

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ NestedResources::Application.routes.draw do
   devise_for :users
   root to: 'posts#index'
 
-  # resources :posts, only: [:index, :new, :create, :show]
-  # resources :comments, only: [:show, :create]
+  post "/posts/:post_id/comments/:id", to: "comments#create_comment", as: "create_comment"
 
   resources :posts do
     resources :comments, only: [:show, :create]
