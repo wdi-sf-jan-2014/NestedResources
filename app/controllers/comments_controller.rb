04331829773e887cdf6430006d29facc28ba8874
Eric Perez <ericricardoperez@gmail.com>
2014-02-12 21:07:39 -0800

    create comments to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,23 @@
 class CommentsController < ApplicationController
-	before_filter :authenticate_user!
+	before_filter :authenticate_user!, only: [:create, :comment_create]
+
+	def show
+		@post = Post.find(params[:post_id])
+		@comment = Comment.find(params[:id])
+	end
 
 	def create
 		post = Post.find(params[:post_id])
 		comment = post.comments.create(params[:comment].permit(:body))
 		current_user.comments << comment
 		redirect_to post_path(post)
+	end
+
+	def comment_create
+		comment = Comment.find(params[:id])
+		new_comment = comment.comments.create(params[:comment].permit(:body))
+		current_user.comments << new_comment
+
+		redirect_to "/posts/#{params[:post_id]}/comments/#{comment.id}"
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 NestedResources::Application.routes.draw do
   root to: "posts#index"
   devise_for :users
+  post "/posts/:post_id/new_comment/:id", to: "comments#comment_create"
 
   resources :posts do 
   	resources :comments 
