5caf77c74f90a8c3048122e49eb84d3d0b7a086d
mbetts7 <mbetts7@gmail.com>
2014-02-12 16:35:38 -0800

    set up models, before migration

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,17 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  body       :string(255)
+#  post_id    :integer
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
+  belongs_to :commentable, polymorphic: true
 end
