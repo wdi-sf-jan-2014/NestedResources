ab84dbf5f5b1225db72e5a7271b07f46b9d1278e
cbestiva <cbestiva@gmail.com>
2014-02-12 18:44:00 -0800

    updated comments controller and views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,28 +1,43 @@
 class CommentsController < ApplicationController
+  before_filter :get_parent
+
   def index
     @post = Post.find(params[:post_id])
   end
 
   def new
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.build
+    # @post = Post.find(params[:post_id])
+    @comment = @parent.comments.build
   end
 
   def show
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.find(params[:id])
+    @comment = @parent.comments.find(params[:id])
   end
 
   def create
-    post = Post.find(params[:post_id])
-    new_comment = params.require(:comment).permit(:body)
-    comment = post.comments.create(new_comment)
-    redirect_to post_path(post)
+    # post = Post.find(params[:post_id])
+    # new_comment = params.require(:comment).permit(:body)
+    # comment = post.comments.create(new_comment)
+    # redirect_to post_path(post)
+
+    @comment = @parent.comments.build(params[:comment])
+    if @comment.save
+      redirect_to post_path(@comment.post)
+    else
+      render :new
+    end
   end
 
   def update
   end
 
   def edit
+  end
+
+  protected
+  def get_parent
+    @parent = Post.find_by_id(params[:post_id]) if params[:post_id]
+    @parent = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140213014905) do
+ActiveRecord::Schema.define(version: 20140213020530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 20140213014905) do
     t.string   "commentable_type"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "comment_id"
   end
 
   add_index "comments", ["commentable_id", "commentable_type"], name: "index_comments_on_commentable_id_and_commentable_type", using: :btree
