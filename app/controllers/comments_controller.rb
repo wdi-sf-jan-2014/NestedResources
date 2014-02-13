6f8f96a58022a213c14d711ab1410db919e0117e
afleisch <afleisch@gmail.com>
2014-02-12 17:16:18 -0800

    added additional comment methods to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,29 @@
 class CommentsController < ApplicationController
   before_filter :authenticate_user!
-  def show
-    @comment = Comment.find(params[:id])
+  
+
+  def index
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments
   end
+
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build
+  end
+
+  def create
+    post = Post.find(params[:post_id])
+    new_comment = params.require(:comment).permit(:body)
+    comment = post.comments.create(new_comment)
+    redirect_to post_comment_path(post, comment)
+  end
+
+  def show
+    @post = Post.find(params[:post_id])
+    comments = @post.comments
+    comment_id = params[:id]
+    @comment = comments.find(comment_id)
+  end
+  
 end
