ef3f989b8d53d4999c26776ca608d41364401611
afleisch <afleisch@gmail.com>
2014-02-13 00:29:37 -0800

    added commentable to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  has_many :comments
-  accepts_nested_attributes_for :comments
+  has_many :comments, as: :commentable, dependent: :destroy
+  accepts_nested_attributes_for :comments 
 end
