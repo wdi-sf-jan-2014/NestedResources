8fa1d697b222518cafea8a8948edb17d83f0b73c
ydarbleoj <brady.joel@gmail.com>
2014-02-12 17:21:30 -0800

    finished controllers

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140211224156) do
+ActiveRecord::Schema.define(version: 20140212030622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
+    t.integer  "post_id"
+    t.integer  "user_id"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", force: true do |t|
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "title"
+    t.text     "link"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
