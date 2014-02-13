d19970e4cf3cbb0982af7074314822f3a93a614a
Whit Schrader <whit@schraderlabs.com>
2014-02-13 11:22:05 -0800

    adding new comments of comments functionality, still not fxnl

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 NestedResources::Application.routes.draw do
   devise_for :users
 
-  resources :posts, only: [:index, :new, :create, :show]
-  resources :comments, only: [:show]
+  resources :posts do
+  	resources :comments
+  end
+
+  # resources :posts, only: [:index, :new, :create, :show]
+  # resources :comments, only: [:show]
   root to: 'posts#index'
 end
