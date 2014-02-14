b44e6fcb879ba383f0f4dfb1854d6d70406d6781
Eric Perez <ericricardoperez@gmail.com>
2014-02-14 12:18:58 -0800

    simple method that goes through the stack of comments of comments starting form a post

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def print_comments(comment, tab=0)
+    puts "\t"*tab + comment.body
+    unless comment.comments.blank?
+      tab += 1
+      comment.comments.each do |c|
+        print_comments(c, tab)
+      end
+    end
+    nil
+  end
+  
 end
